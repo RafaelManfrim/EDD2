f9cd197e7a3a33befef5adad2c68898958cc1ae2
Rafael Manfrim <rafaelmanfrim2004@gmail.com>
2024-12-02 20:29:13 -0300

    Aula 3 e aula 7 finalizadas

--- Exercícios/aula07/arvore_binaria_de_busca.cpp
+++ Exercícios/aula07/arvore_binaria_de_busca.cpp
@@ -35,9 +35,9 @@ No* BuscaNaArvore(No* no, int valor) {
 
 No* BuscaRecursivaNaArvore(No* no, int valor) {
     if(valor < no->valor) {
-        BuscaRecursivaNaArvore(no->no_esq, valor);
+        return BuscaRecursivaNaArvore(no->no_esq, valor);
     } else if (valor > no->valor) {
-        BuscaRecursivaNaArvore(no->no_dir, valor);
+        return BuscaRecursivaNaArvore(no->no_dir, valor);
     } else {
         return no;
     }
@@ -75,6 +75,32 @@ No* ArvoreMaximoRecursiva(No* no) {
     }
 }
 
+No* EncontraSucessor(No* no) {
+    if(no->no_dir != nullptr) {
+        return ArvoreMinimo(no->no_dir);
+    }
+
+    No* no_pai = no->no_pai;
+    while (no_pai != nullptr && no == no_pai->no_dir) {
+        no = no_pai;
+        no_pai = no_pai->no_pai;
+    }
+    return no_pai;
+}
+
+No* EncontraPredecessor(No* no) {
+    if(no->no_esq != nullptr) {
+        return ArvoreMaximo(no->no_esq);
+    }
+
+    No* no_pai = no->no_pai;
+    while (no_pai != nullptr && no == no_pai->no_esq) {
+        no = no_pai;
+        no_pai = no_pai->no_pai;
+    }
+    return no_pai;
+}
+
 void InserirNaArvore(ArvoreBinariaBusca* arvore, No* no) {
     No* no_atual = arvore->raiz;
 
@@ -101,6 +127,21 @@ void InserirNaArvore(ArvoreBinariaBusca* arvore, No* no) {
 
         no->no_pai = no_pai;
     }
+}
+
+No* InserirNaArvoreRecursiva(No* subarvore, No* no_pai, No* no) {
+    if(subarvore == nullptr) {
+        no->no_pai = no_pai;
+        return no;
+    }
+
+    if(no->valor < subarvore->valor) {
+        subarvore->no_esq = InserirNaArvoreRecursiva(subarvore->no_esq, subarvore, no);
+    } else if(no->valor > subarvore->valor) {
+        subarvore->no_dir = InserirNaArvoreRecursiva(subarvore->no_dir, subarvore, no);
+    }
+
+    return subarvore;
 }
 
 void SubstituiSubarvore(ArvoreBinariaBusca* arvore, No* no_substituido, No* no_substituir) {
@@ -192,14 +233,35 @@ int main() {
 
     cout << endl;
 
-    No* no_buscado_recursivamente = BuscaNaArvore(arvore->raiz, 8);
+    No* no_buscado_recursivamente = BuscaRecursivaNaArvore(arvore->raiz, 8);
     cout << "Posição na memória do nó buscado recursivamente: " << no_buscado_recursivamente << ", valor: " << no_buscado_recursivamente->valor << endl;
+
+    No* predecessor = EncontraPredecessor(no_buscado_recursivamente);
+    cout << "Posição na memória do predecessor do nó buscado recursivamente: " << predecessor << ", valor: " << predecessor->valor << endl;
+
+    No* sucessor = EncontraSucessor(no_buscado_recursivamente);
+    cout << "Posição na memória do sucessor do nó buscado recursivamente: " << sucessor << ", valor: " << sucessor->valor << endl;
 
     no_menor = ArvoreMinimoRecursivo(arvore->raiz);
     cout << "Posição na memória do menor nó: " << no_menor << ", valor: " << no_menor->valor << endl;
 
     no_maior = ArvoreMaximoRecursiva(arvore->raiz);
     cout << "Posição na memória do maior nó: " << no_maior << ", valor: " << no_maior->valor << endl;
+
+    no = new No();
+    CriaNo(no, 19);
+    InserirNaArvoreRecursiva(arvore->raiz, nullptr, no);
+
+    no = new No();
+    CriaNo(no, 2);
+    InserirNaArvoreRecursiva(arvore->raiz, nullptr, no);
+
+    no = new No();
+    CriaNo(no, 6);
+    InserirNaArvoreRecursiva(arvore->raiz, nullptr, no);
+
+    cout << "Percurso em Ordem depois de inserir recursivamente: ";
+    PercursoEmOrdem(arvore->raiz);
 
     delete arvore;
 
